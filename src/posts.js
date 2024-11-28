912af627d9b8ff7ccafe64097a582f36a2006855
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-28 17:07:36 +0100

    change value to try

--- src/posts.js
+++ src/posts.js
@@ -21,7 +21,7 @@ export const posts = [
 	{
 		id: 3,
 		title: 'Titolo del Post',
-		image: gto,
+		image: '',
 		content:
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.',
 		tags: ['js', 'php'],
