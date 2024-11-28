43cbe5c84948cb392bdfc13d86539b13121b310e
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-28 14:39:44 +0100

    change the main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,33 +1,25 @@
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import { Col, Row, Container } from "react-bootstrap";
+import { Row, Container } from "react-bootstrap";
 import style from "./Main.module.css"
+import { posts } from "../../posts.js"
+import { PostCard } from "./PostCard.jsx";
 export function Main() {
   return (
     <div className={`${style.bgcolor_lightGrey} ${style.flex_grow_1}`}>
       <Container>
-        <Row
-          className={[
-            style.justify_center,
-            style.align_center,
-            style.padding_top_50
-          ]}
-        >
-          <Col xs={6}>
-            <Card>
-              <Card.Img src="/images/gto2.jpg" />
-              <Card.Body>
-                <Card.Title>Titolo del post</Card.Title>
-                <Card.Text>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic
-                  architecto doloremque provident velit saepe unde officia,
-                  ipsum suscipit mollitia? Eius, ratione maxime. Qui provident
-                  debitis, doloribus nobis quis voluptatem maxime.
-                </Card.Text>
-                <Button variant="primary">Leggi di piu</Button>
-              </Card.Body>
-            </Card>
-          </Col>
+        <Row className={[style.justify_center, style.align_center, style.padding_top_50]}>
+          {posts.map((el, i)=>{
+            return(
+              <div key = {i} className="col-12 col-sm-6 col-md-4 col-xl-2">
+                <PostCard 
+                id= {el.id}
+                title = {el.title}
+                image = {el.image}
+                content = {el.content}
+                tags = {el.tags}
+                published = {el.published}/>
+              </div> 
+            )
+          })}
         </Row>
       </Container>
     </div>
